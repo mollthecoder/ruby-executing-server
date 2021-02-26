c66d3e3929de38803659027298553cf94549187c
mollthecoder <zackeryspry@mail.com>
2021-02-26 16:04:51 +0000

    Now redirects to https and has an index.html

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const app = require("express")();
 const path = require("path")
 const fs = require("fs");
 const {exec} = require("child_process");
+app.use((req, res, next)=>{
+  // HTTPS redirect changed from https://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
+  if(req.secure) {
+    next();
+  }else{
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
 app.get("/", (req, res)=>{
-  res.send("No index file.");
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 app.get("*", (req, res)=>{
   if(fs.existsSync(path.join("public", req.url))){
